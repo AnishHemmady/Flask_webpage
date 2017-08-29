9db5aeb052ef71921748d3c9bffb0a22cec43cee
AnishHemmady <anhemmady@gmail.com>
2017-08-28 21:31:00 -0400

    initial_commit

---
+++ app/__init__.py
@@ -0,0 +1,8 @@
+'''__init__.py'''
+
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+from app import views
---
+++ app/views.py
@@ -0,0 +1,25 @@
+from flask import render_template,flash, redirect
+from app import app
+from .forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index_page():
+	user={'name':"ANISH"}
+	posts=[{ 'author':{'name':"Rahul"},
+			'body':'Good day in Indy'},
+			{ 'author':{'name':"Vijay"},
+			'body':'Good day in Ohio'}
+			]
+	return render_template("index.html",title="Home",posts=posts,user=user)
+	
+
+@app.route('/login',methods=['GET','POST'])
+def login():	
+	form=LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="%s", remember_me=%s' %
+			  (form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html',title='Sign In',form=form)
+
